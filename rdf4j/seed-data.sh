b66ac995b7bd351d5698642579875645291e4524
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2019-10-02 11:56:10 +1000

    naming conventions

--- rdf4j/seed-data.sh
+++ rdf4j/seed-data.sh
@@ -1,4 +1,4 @@
-echo -e "connect http://rdf4j:8080/rdf4j-server \n\
+echo -e "connect http://phis-rdf4j:8080/rdf4j-server \n\
 create native \n\
 phis \n\
 phis \n\n\n\n\
@@ -6,4 +6,4 @@ open phis \n\
 load /tmp/oeso.owl into urn:http://www.opensilex.org/vocabulary/oeso \n\
 load /tmp/oeev.owl into urn:http://www.opensilex.org/vocabulary/oeev \n\
 load /tmp/oa.rdf into urn:http://www.w3.org/ns/oa \n\
-exit" | /tmp/eclipse-rdf4j-2.5.2/bin/console.sh
+exit" | /tmp/eclipse-rdf4j-3.0.0/bin/console.sh
